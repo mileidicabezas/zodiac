11205f792be8ebb3b96d9c8a75652af6b696e700
mileidicabezas <mi.ladydi@hotmail.com>
2021-04-07 14:52:28 +0100

    add views  of the appp

---
+++ zodiacsing/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def zodiac(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/zodiac.html")
+
+def horoscopo(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/horoscopo.html")
+    
+def aries(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/aries.html")
+
+def leo(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/leo.html")
+
+def sagittarius(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/sagittarius.html")
+
+def taurus(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/taurus.html")
+
+def virgo(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/virgo.html")
+
+def capricorn(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/capricorn.html")
+
+def gemini(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/gemini.html")
+
+def libra(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/libra.html")
+
+def aquarius(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/aquarius.html")
+
+def cancer(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/cancer.html")
+
+def scorpio(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/scorpio.html")
+
+def pisces(request):
+    return render(request, "/Users/mileidicabezas/dev/webprojects/zodiac/zodiacsing/templates/zodiacsing/pisces.html")
+
